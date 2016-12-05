078f4b96b832cced66f5cfea9594f558100e2ca1
ODabrowski <od_1986@hotmail.com>
2016-12-05 17:29:46 +0100

    'v3'

--- puzzle1.py
+++ puzzle1.py
@@ -3,21 +3,6 @@ from turtle import *
 #Oscar Dabrowski
 #Advent puzzle 1
 
-#translate R L combinations into R,L,U,D
-conversion_dic_L={
-'L' : 'D',
-'D' : 'R',
-'R' : 'U',
-'U' : 'L',
-}
-
-conversion_dic_R={
-'R' : 'D',
-'D' : 'L',
-'L' : 'U',
-'U' : 'R',
-}
-
 def rotate_vect(v,angle):
     v_res=[0,0]
     rot_matrix=[[math.cos(angle), -1*math.sin(angle)],[math.sin(angle),math.cos(angle)]]
@@ -26,8 +11,6 @@ def rotate_vect(v,angle):
             v_res[i]+=int(rot_matrix[i][j]*v[j])
     return v_res
         
-
-
 def scalar_product(v1,v2):
     res=0
     for i in range(len(v1)):
@@ -46,42 +29,47 @@ def proj_a_b(a,b):
     proj=scalar_product(a,b)/norm_vect(b)
     return proj
 
-def disp_vect(dist,curr_dir,prev_dir):
-    v_res=[0, 0]
-    new_dir=''
-    #from current and previous directions, find next direction
-    if prev_dir=='R':
-        new_dir=conversion_dic_R[curr_dir]
-    elif prev_dir=='L':
-        new_dir=conversion_dic_L[curr_dir]
-    else:
-        new_dir=curr_dir
-    #compute vector from direction
-    if new_dir=='R':
-        v_res=[1, 0]
-    elif new_dir=='L':
-        v_res=[-1, 0]
-    elif new_dir=='U':
-        v_res=[0, 1]
-    elif new_dir=='D':
-        v_res=[0, -1]
-    #comput final displacement vector
-    for i in range(len(v_res)):
-        v_res[i]=v_res[i]*dist
-    return [v_res, new_dir]
+def add_vect(v1,v2):
+    v_res=[0,0]
+    if len(v1)!=len(v2):
+        print('error : vectors have same length!')
+    for i in range(len(v1)):
+        v_res[i]+=v1[i]+v2[i]
+    return v_res
+        
 
-#previous dist set initially to Up as dic_L(U)->L and dic_R(U)->R
-#initially equivalent to first direction from beginning
-p_dist=''
-c_dist=''
+#Compute displacement vector.
+#if same direction between 2 calls of the function e.g. R->R or L->L in the puzzle,
+#then do a 90 degrees rot else if different e.g. R->L or L->R do a -90 degrees rot
+def disp_vect(v,curr_dir,prev_dir):
+    v_res=[]
+    if prev_dir=='I':
+        #convention for initial
+        if curr_dir=='R':
+            v_res=[1,0]
+        elif curr_dif=='L':
+            v_res=[-1,0]
+    else:
+        if prev_dir != curr_dir:
+            #-90 degree rotation
+            v_res=rotate_vect(v,-math.pi/2)
+        else:
+            #-90 degree rotation
+            v_res=rotate_vect(v,math.pi/2)
+    return v_res
+
+p_dir='I'#initial, special case
+c_dir=''
 d_vect=[0, 0]
-r_vect=[0, 0]
+v=[0,1]#initial vector
 lst=''
 lst2=[]
 direction=''
+
 with open('puzzle1.txt','r') as f:
     for ln in f:
         lst=lst+ln
+
 lst=lst.split(',')
 for i in lst:
     lst2.append(i.strip())#remove spaces
@@ -90,26 +78,28 @@ for i in lst:
 #print(norm_vect([1,1]))
 for i in lst2:
     d=i[1:]
-    c_dist=i[0]
-    #vector for computing L1 norm
-    d_vect=disp_vect(int(d),c_dist,p_dist)
+    c_dir=i[0]
+    #total displacement vector for computing L1 norm
+    d_vect=add_vect(d_vect,disp_vect(v,c_dir,p_dir))
+    print('d_v=',d_vect)
     #dir for turtle
     direction=i[0]#d_vect[1]
-    p_dist=c_dist#old = new - but with regards to only L or R (not U,D)
+    p_dir=c_dir#old = new
     #print('d',d,'c_dist=',c_dist,'p_dist=',p_dist)
     #print('d_vect=',d_vect[0])
-    #turtle display
-    #print('dir=',d_vect[1])
-##    if direction=='R':
-##        right(90)
-##        forward(int(d))
-##    elif direction=='L':
-##        right(90)
-##        forward(int(d))
-##    elif direction=='U':
-##        forward(int(d))
-##    elif direction=='D':
-##        backward(int(d))
-        
+    #turtle path display
+    speed(20)
+    if direction=='R':
+        right(90)
+        forward(int(d))
+    elif direction=='L':
+        left(90)
+        forward(int(d))
+
+goto(0,0)
+color("red")
+goto(d_vect[0],d_vect[1])
+#print(d_vect)
 #print(rotate_vect([1,1],-math.pi/2))
+#print('test=',add_vect([1,2],[-5,1]))
 done()
